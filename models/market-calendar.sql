a6418fad455e4767de7f01a0c902d342c3c56660
sgoley <scottmgoley@gmail.com>
2021-09-18 13:58:30 -0400

    template model for calendar

---
+++ models/market-calendar.sql
@@ -0,0 +1,20 @@
+select 
+      csv.Row
+    , csv.Date
+    , csv.Year
+    , csv.Month
+    , csv.YearMonth
+    , csv.Day
+    , csv.Week_Index
+    , csv.Day_Index
+    , csv.is_weekend
+    , csv.is_market_holiday
+    , csv.is_eq_weekly_exp
+    , csv.eq_weekly_exp_cumulative
+    , csv.is_eq_monthly_exp
+    , csv.is_eq_quarterly_exp
+    , csv.is_vix_weekly_exp
+    , csv.vix_weekly_exp_cumulative
+    , csv.is_vix_monthly_exp
+    , csv.is_vix_quarterly_exp
+from {{ ref('market-calendar-csv') }} csv
